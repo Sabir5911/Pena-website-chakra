a3a6a197cb2eb7d56d4794d0ffa52b78acbca69a
Sabir5911 <115262495+Sabir5911@users.noreply.github.com>
2023-02-16 01:40:25 +0500

    2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,14 +10,14 @@ export default function Footer() {
             <Flex flexDirection={'column'} gap={{base:"9px",sm:"25px",lg:"30px"}} ml={{base:"30px",sm:"75px",lg:"200px"}} mt={{base:"60px",sm:"150px",lg:"200px"}} fontFamily={'inherit'}>
                 <Box  >
 
-                    <Heading color={'teal'} fontSize={{base:"15px",sm:"25px",lg:"45px"}}>
+                    <Heading color={'teal'} fontSize={{base:"15px",sm:"25px",lg:"40px"}}>
                         <Text color={'white'}>Become</Text>
                         Certified Web 3.0 <Text color={'white'} display='inline'>and</Text> <br />
                         Metaverse Developer
 
                     </Heading>
 
-                    <Text color={'white'} fontSize={{base:"8px",sm:"14px",lg:"28px"}} mt={{base:"10px",sm:"5px",lg:"25px"}}>launch Your Dev Career With Web 3 and Metaverse <br />
+                    <Text color={'white'} fontSize={{base:"8px",sm:"14px",lg:"20px"}} mt={{base:"10px",sm:"5px",lg:"25px"}}>launch Your Dev Career With Web 3 and Metaverse <br />
                         Getting Ready for the Next Generation of the <br /> Internet</Text>
                 </Box>
 
@@ -27,7 +27,7 @@ export default function Footer() {
 
             </Flex>
 
-            <Flex justifyContent={'right'} mt={{base:"-12rem",sm:"-18rem",lg:"-29rem"}} mr={{base:"-13px",sm:"-40px",lg:"8px"}}>
+            <Flex justifyContent={'right'} mt={{base:"-12rem",sm:"-18rem",lg:"-23rem"}} mr={{base:"6px",sm:"0px",lg:"8px"}}>
                 <Image src='banner2.svg' alt='logo' width={{base:"180px",sm:"350px",lg:"550px"}}/>
 
             </Flex>
